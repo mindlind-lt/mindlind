d4d20a80dc74d173a6661ee8e8abedc7e4780f08
Almaz Bisenbaev <almaz9527@gmail.com>
2026-03-26 17:28:48 +0500

    Disable indexing

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,20 @@
+// Disable indexing
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://new.mindlind.de/sitemap.xml",
+    rules: {
+      userAgent: "*",
+      disallow: "/",
+    },
   };
 }
+
+// import type { MetadataRoute } from "next";
+
+// export default function robots(): MetadataRoute.Robots {
+//   return {
+//     rules: { userAgent: "*", allow: "/" },
+//     sitemap: "https://new.mindlind.de/sitemap.xml",
+//   };
+// }
