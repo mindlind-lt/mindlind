4398d1bf317aaa452f3cbb5b5ea3697dc7e86d42
Almaz Bisenbaev <almaz9527@gmail.com>
2026-05-22 06:02:21 +0500

    Deobfuscate js

--- assets/js/augiA20Il.BrGn5QaW.mjs
+++ assets/js/augiA20Il.BrGn5QaW.mjs
@@ -1,2 +1,41 @@
-import{t as e}from"./rolldown-runtime.DsXBSD_B.mjs";import{n as t,t as n}from"./shared-lib.Dhk_iVEJ.mjs";function r(e,n){return{breakpoints:[{hash:`72rtr7`,mediaQuery:`(min-width: 1200px)`},{hash:`7wmc59`,mediaQuery:`(min-width: 810px) and (max-width: 1199.98px)`},{hash:`iigsnu`,mediaQuery:`(max-width: 809.98px)`}],description:t(e,n).description,elements:{},robots:`max-image-preview:large`,serializationId:`framer-kIudB`,title:t(e,n).title||`Home`,viewport:`width=device-width`}}var i,a,o=e((()=>{n(),i=1,a={exports:{metadataVersion:{type:`variable`,annotations:{framerContractVersion:`1`}},default:{type:`function`,annotations:{framerContractVersion:`1`}},__FramerMetadata__:{type:`variable`}}}}));export{i,o as n,r,a as t};
+import { t as e } from "./rolldown-runtime.DsXBSD_B.mjs";
+import { n as t, t as n } from "./shared-lib.Dhk_iVEJ.mjs";
+function r(e, n) {
+	return {
+		breakpoints: [
+			{ hash: `72rtr7`, mediaQuery: `(min-width: 1200px)` },
+			{
+				hash: `7wmc59`,
+				mediaQuery: `(min-width: 810px) and (max-width: 1199.98px)`,
+			},
+			{ hash: `iigsnu`, mediaQuery: `(max-width: 809.98px)` },
+		],
+		description: t(e, n).description,
+		elements: {},
+		robots: `max-image-preview:large`,
+		serializationId: `framer-kIudB`,
+		title: t(e, n).title || `Home`,
+		viewport: `width=device-width`,
+	};
+}
+var i,
+	a,
+	o = e(() => {
+		(n(),
+			(i = 1),
+			(a = {
+				exports: {
+					metadataVersion: {
+						type: `variable`,
+						annotations: { framerContractVersion: `1` },
+					},
+					default: {
+						type: `function`,
+						annotations: { framerContractVersion: `1` },
+					},
+					__FramerMetadata__: { type: `variable` },
+				},
+			}));
+	});
+export { i, o as n, r, a as t };
 //# sourceMappingURL=augiA20Il.BrGn5QaW.mjs.map
