ddbb536f4bf1a660c6d53c1338df241627afbce5
Almaz Bisenbaev <almaz9527@gmail.com>
2026-06-04 16:09:37 +0500

    Style menu

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import "./header.css";
+
 export default function Header() {
   return (
     <header className="hdr">
@@ -22,125 +24,7 @@ export default function Header() {
         <a href="/contact">Contact</a>
       </nav>
 
-      <div className="drawer-container">
-        <nav
-          className="framer-GPpH2 framer-wMbOK framer-njEjc framer-jzp9vj framer-v-13f1p5r"
-        >
-          <div
-            className="framer-14vltbg"
-            style={{
-              backgroundColor: "rgb(255, 255, 255)",
-              borderRadius: "8px 0px 0px 8px",
-              boxShadow: "rgba(0, 0, 0, 0.03) 0px 0px 50px 0px",
-              transform: "none",
-              transformOrigin: "50% 50% 0px",
-              willChange: "auto",
-            }}
-          >
-
-            {/* Menu */}
-            <div
-              className="framer-1qmhc63"
-              data-framer-name="Menu"
-              style={{ willChange: "transform", opacity: 1, transform: "none", transformOrigin: "50% 50% 0px" }}
-            >
-              <div className="framer-1gcqi0d-container" style={{ opacity: 1 }}>
-                <a className="framer-3ytSH framer-oi3Va framer-zeChO framer-1dqtob3 framer-v-tr4n4e framer-y7m2kj" data-framer-name="Big" data-highlight="true" href="./demo-1" data-framer-page-link-current="true" tabIndex={0} style={{ opacity: 1 }}>
-                  <div className="framer-5oty5o" data-framer-name="Home" data-framer-component-type="RichTextContainer" style={{ transform: "none", transformOrigin: "50% 50% 0px" }}>
-                    <h6 className="framer-text framer-styles-preset-nkm6se" data-styles-preset="BaUGQBb6A" style={{ textAlign: "left" }}>Home</h6>
-                  </div>
-                </a>
-              </div>
-              <div className="framer-jayq2i-container" style={{ opacity: 1 }}>
-                <a className="framer-3ytSH framer-oi3Va framer-zeChO framer-1dqtob3 framer-v-tr4n4e framer-y7m2kj" data-framer-name="Big" data-highlight="true" href="./demo-1/agency" tabIndex={0} style={{ opacity: 1 }}>
-                  <div className="framer-5oty5o" data-framer-name="Home" data-framer-component-type="RichTextContainer" style={{ transform: "none", opacity: 1 }}>
-                    <h6 className="framer-text framer-styles-preset-nkm6se" data-styles-preset="BaUGQBb6A" style={{ textAlign: "left" }}>Agency</h6>
-                  </div>
-                </a>
-              </div>
-              <div className="framer-27yli9-container" style={{ opacity: 1 }}>
-                <a className="framer-3ytSH framer-oi3Va framer-zeChO framer-1dqtob3 framer-v-tr4n4e framer-y7m2kj" data-framer-name="Big" data-highlight="true" href="./demo-1/projects" tabIndex={0} style={{ opacity: 1 }}>
-                  <div className="framer-5oty5o" data-framer-name="Home" data-framer-component-type="RichTextContainer" style={{ transform: "none", opacity: 1 }}>
-                    <h6 className="framer-text framer-styles-preset-nkm6se" data-styles-preset="BaUGQBb6A" style={{ textAlign: "left" }}>Projects</h6>
-                  </div>
-                </a>
-              </div>
-              <div className="framer-cylgke-container" style={{ opacity: 1 }}>
-                <a className="framer-3ytSH framer-oi3Va framer-zeChO framer-1dqtob3 framer-v-tr4n4e framer-y7m2kj" data-framer-name="Big" data-highlight="true" href="./demo-1/blog" tabIndex={0} style={{ opacity: 1 }}>
-                  <div className="framer-5oty5o" data-framer-name="Home" data-framer-component-type="RichTextContainer" style={{ transform: "none", opacity: 1 }}>
-                    <h6 className="framer-text framer-styles-preset-nkm6se" data-styles-preset="BaUGQBb6A" style={{ textAlign: "left" }}>Blog</h6>
-                  </div>
-                </a>
-              </div>
-              <div className="framer-puxj5z-container" style={{ opacity: 1 }}>
-                <a className="framer-3ytSH framer-oi3Va framer-zeChO framer-1dqtob3 framer-v-tr4n4e framer-y7m2kj" data-framer-name="Big" data-highlight="true" href="./demo-1/contact" tabIndex={0} style={{ opacity: 1 }}>
-                  <div className="framer-5oty5o" data-framer-name="Home" data-framer-component-type="RichTextContainer" style={{ transform: "none", opacity: 1 }}>
-                    <h6 className="framer-text framer-styles-preset-nkm6se" data-styles-preset="BaUGQBb6A" style={{ textAlign: "left" }}>Contact</h6>
-                  </div>
-                </a>
-              </div>
-            </div>
-
-            {/* Address */}
-            <div className="framer-kfnkjr" data-framer-name="Address" style={{ willChange: "transform", opacity: 1, transform: "none", transformOrigin: "50% 50% 0px" }}>
-              <div className="framer-ts9bfi" data-framer-component-type="RichTextContainer" style={{ transform: "none", opacity: 1 }}>
-                <h6 className="framer-text framer-styles-preset-41ki07" data-styles-preset="hmRgK3K6v">
-                  <a className="framer-text framer-styles-preset-mgrqd0" data-styles-preset="q7NcKTKUL" href="https://maps.app.goo.gl/mFzHrdjVoW5kQEgB6" target="_blank" rel="noopener noreferrer">27 Whitfield Yard, Shoreditch, London E2 7NX</a>
-                </h6>
-              </div>
-              <div className="framer-118e5g3" data-framer-component-type="RichTextContainer" style={{ transform: "none", opacity: 1 }}>
-                <h6 className="framer-text framer-styles-preset-41ki07" data-styles-preset="hmRgK3K6v">
-                  <a className="framer-text framer-styles-preset-mgrqd0" data-styles-preset="q7NcKTKUL" href="tel:+442039991245" target="_blank" rel="noopener noreferrer">+44 20 3999 1245</a>
-                </h6>
-              </div>
-            </div>
-
-            {/* Follow Us */}
-            <div className="framer-w5n2jb" data-framer-name="Follow Us" style={{ willChange: "transform", opacity: 1, transform: "none", transformOrigin: "50% 50% 0px" }}>
-              <div className="framer-1s4gov" data-framer-component-type="RichTextContainer" style={{ transform: "none", opacity: 1 }}>
-                <h6 className="framer-text framer-styles-preset-41ki07" data-styles-preset="hmRgK3K6v">Follow Us</h6>
-              </div>
-              <div className="framer-1nbtl7m-container" style={{ opacity: 1 }}>
-                <div className="framer-vvcUa framer-173n7xw framer-v-173n7xw" data-framer-name="Variant 1" style={{ width: "100%", opacity: 1 }}>
-                  <div className="framer-y0u1op-container" style={{ opacity: 1 }}>
-                    <a className="framer-FJV3P framer-gaqzk1 framer-v-gaqzk1 framer-f16gsk" data-framer-name="Variant 1" data-highlight="true" href="https://contra.com/" target="_blank" rel="noopener noreferrer" style={{ height: "100%", width: "100%", opacity: 1 }}>
-                      <img data-framer-name="Icon" alt="Contra" className="framer-A3lb6 framer-jq73x8" src="data:image/svg+xml,..." style={{ opacity: 1 }} />
-                    </a>
-                  </div>
-                  <div className="framer-1iwg1bv-container" style={{ opacity: 1 }}>
-                    <a className="framer-FJV3P framer-gaqzk1 framer-v-gaqzk1 framer-f16gsk" data-framer-name="Variant 1" data-highlight="true" href="https://x.com/" target="_blank" rel="noopener noreferrer" style={{ height: "100%", width: "100%", opacity: 1 }}>
-                      <img data-framer-name="Icon" alt="X" className="framer-gPUmW framer-jq73x8" src="data:image/svg+xml,..." style={{ opacity: 1 }} />
-                    </a>
-                  </div>
-                  <div className="framer-i7m4r0-container" style={{ opacity: 1 }}>
-                    <a className="framer-FJV3P framer-gaqzk1 framer-v-gaqzk1 framer-f16gsk" data-framer-name="Variant 1" data-highlight="true" href="https://www.behance.net/" target="_blank" rel="noopener noreferrer" style={{ height: "100%", width: "100%", opacity: 1 }}>
-                      <img data-framer-name="Icon" alt="Behance" className="framer-1kxPX framer-jq73x8" src="data:image/svg+xml,..." style={{ opacity: 1 }} />
-                    </a>
-                  </div>
-                  <div className="framer-grshjn-container" style={{ opacity: 1 }}>
-                    <a className="framer-FJV3P framer-gaqzk1 framer-v-gaqzk1 framer-f16gsk" data-framer-name="Variant 1" data-highlight="true" href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" style={{ height: "100%", width: "100%", opacity: 1 }}>
-                      <img data-framer-name="Icon" alt="Instagram" className="framer-TwpIa framer-jq73x8" src="data:image/svg+xml,..." style={{ opacity: 1 }} />
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-          {/* Burger Button */}
-          <div className="drawer-burger" style={{ background: "#fff", transform: "none", transformOrigin: "50% 50% 0px" }}>
-            <div className="drawer-burger-inner framer-1s7c67u framer-v-1fu7lbd" data-highlight="true" tabIndex={0} style={{ height: "100%", width: "100%", transform: "none", transformOrigin: "50% 50% 0px", opacity: 1 }} data-framer-name="Open - Close">
-              <div className="framer-tig9p8" style={{ transform: "none", transformOrigin: "50% 50% 0px", opacity: 1 }}>
-                <div className="framer-1lh8ck8" data-framer-name="Rectangle 4" style={{ backgroundColor: "var(--token-3cdc6fa6-d3d3-48ad-b744-7855447ef4d2, rgb(0, 0, 0))", transform: "rotate(30deg)", transformOrigin: "50% 50% 0px", willChange: "transform", opacity: 1 }} />
-                <div className="framer-xvj3te" data-framer-name="Rectangle 5" style={{ backgroundColor: "var(--token-3cdc6fa6-d3d3-48ad-b744-7855447ef4d2, rgb(0, 0, 0))", opacity: 0, transform: "none", transformOrigin: "50% 50% 0px", willChange: "transform" }} />
-                <div className="framer-99r2te" data-framer-name="Rectangle 6" style={{ backgroundColor: "var(--token-3cdc6fa6-d3d3-48ad-b744-7855447ef4d2, rgb(0, 0, 0))", transform: "rotate(-30deg)", transformOrigin: "50% 50% 0px", willChange: "transform", opacity: 1 }} />
-              </div>
-            </div>
-          </div>
-
-        </nav>
-      </div>
+      <div>Burger</div>
 
     </header>
   );
