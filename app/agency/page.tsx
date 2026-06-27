75f84d64b44bd7366993532cabefa63f1c5f8dae
Almaz Bisenbaev <almaz9527@gmail.com>
2026-06-28 03:37:48 +0500

    Add agency page

--- app/agency/page.tsx
+++ app/agency/page.tsx
@@ -1,7 +1,31 @@
 export default function AgencyPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Coming Soon</h1>
-    </main>
+    <>
+    
+
+      <div className="bg-red-100 py-50 items-center justify-center flex">
+        <h1 className="text-4xl font-bold">INTRO FROM SPLINE</h1>
+      </div>
+
+
+      <div className="intro py-20">
+        <div className="container mx-auto px-5">
+          <div className="grid grid-cols-[1fr_360px] gap-4">
+
+            <div className="intro-row">
+              <div className="text-5xl font-mono font-medium uppercase leading-[1.4]">
+                <p><span className="text-3xl text-primary-500">[01]</span>&nbsp;&nbsp;&nbsp;From strategy to launch, we plan, design  and build end-to-end – aligning positioning, content, and modular UI into websites that load fast, meet AA accessibility and are easy to edit.</p>
+                <p className="mt-8">Our DNA: clarity over noise, systems over one-offs, speed with accountability.</p>
+              </div>
+            </div>
+
+            <div className="bg-red-100 py-50 items-center justify-center flex">3D</div>
+
+          </div>
+        </div>
+      </div>
+
+
+    </>
   );
 }
