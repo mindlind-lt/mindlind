2d1f623a5c280061f72c9c3ca979efdee76e4fdb
Almaz Bisenbaev <almaz9527@gmail.com>
2026-07-08 09:18:47 +0500

    b

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,11 +1,21 @@
 import SectionServices from "@/components/section-services/section-services";
+import SectionContact from "@/components/section-contact/section-contact";
 
 export default function PageServices(){
     return (
         <>
 
-            <h1>Leistungen</h1>
-            <div>Unsere Lösungen werden individuell an Ziele, Branche und Wachstumstempo angepasst.</div>
+            <div className="pt-[160px] pb-20">
+                <div className="container mx-auto px-5">
+                    <div className="grid grid-cols-3 gap-20">
+                        <div className="col-span-2">
+                            <h1 className="text-[120px] tracking-tighter font-bold">Leistungen</h1>
+                            <div className="text-3xl mt-6">Unsere Lösungen werden individuell an Ziele, Branche und Wachstumstempo angepasst.</div>
+                        </div>
+                        <div className="bg-red-100 py-50 items-center justify-center flex">3D</div>
+                    </div>
+                </div>
+            </div>
 
             <div className="py-20">
                 <div className="container mx-auto px-5">
@@ -14,6 +24,10 @@ export default function PageServices(){
 
                 </div>
             </div>
+
+
+            <SectionContact />
+
 
         </>
     )
