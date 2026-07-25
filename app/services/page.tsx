3801454635ffba7c63a5d19cc50b357dec3e3f31
Almaz Bisenbaev <almaz9527@gmail.com>
2026-07-26 04:15:13 +0500

    a

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,16 +1,16 @@
+'use client';
+
 import SectionServices from "@/components/section-services/section-services";
 import SectionContact from "@/components/section-contact/section-contact";
-import TorusGlass from "@/components/torus-glass/torus-glass";
 
 export default function PageServices(){
     return (
         <>
 
-            <div className="pt-[160px] pb-20 border-4 border-dashed border-red-500">
-                <div className="container mx-auto px-8">
+            <div className="pt-[160px] pb-20 border-4 border-dashed border-red-500 relative">
+                <div className="container mx-auto px-8 relative z-10">
                     <h1 className="text-[120px] tracking-tighter font-bold">Leistungen</h1>
                     <div className="text-3xl mt-6">Unsere Lösungen werden individuell an Ziele, Branche und Wachstumstempo angepasst.</div>
-                    <TorusGlass className="absolute inset-0" />
                 </div>
             </div>
 
