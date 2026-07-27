f11ec58a5ad991826e0768a55e9f5bc914c06234
Almaz Bisenbaev <almaz9527@gmail.com>
2026-07-27 05:11:23 +0500

    c

--- components/contact-button/contact-button.tsx
+++ components/contact-button/contact-button.tsx
@@ -5,7 +5,7 @@ export default function ContactButton() {
     <a className="fancy-button" href="#">
         <img className="fancy-button-icon fancy-button-icon-1" src="/assets/images/icon-phone.png" alt="" />
         <img className="fancy-button-icon fancy-button-icon-2" src="/assets/images/icon-email.png" alt="" />
-        <img className="fancy-button-icon fancy-button-icon-3" src="/assets/images/cta-icon-1.png" alt="" />
+        <img className="fancy-button-icon fancy-button-icon-3" src="/images/character-1.png" alt="" />
         <div>Contact manager</div>
     </a>
   );
